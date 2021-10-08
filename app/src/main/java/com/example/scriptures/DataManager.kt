e1df6a9d0190302a04ba3cd33a77bfe3c03f15d1
Koose <KooseTheBlogger@gmail.com>
2021-10-08 22:19:51 +0000

    Data Manager and others

---
+++ app/src/main/java/com/example/scriptures/DataManager.kt
@@ -0,0 +1,7 @@
+package com.example.scriptures
+
+class DataManager {
+    val courses = HashMap<String, Courseinfo>()
+    val notes = ArrayList<NoteInfo>()
+
+}
---
+++ app/src/main/java/com/example/scriptures/NoteKeeperData.kt
@@ -0,0 +1,6 @@
+package com.example.scriptures
+
+class Courseinfo(val courseid: String, val title:String)
+
+class NoteInfo(var course: Courseinfo, var title: String, var text:String)
+
